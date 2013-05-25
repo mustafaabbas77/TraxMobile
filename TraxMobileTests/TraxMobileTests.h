97d84bfc939b9fb58e3f5bece3e22e284742f9ac
OPSITMAC <OPSITMAC@mac.home>
2013-05-24 20:23:12 -0500

    Initial Commit

---
+++ TraxMobileTests/TraxMobileTests.h
@@ -0,0 +1,13 @@
+//
+//  TraxMobileTests.h
+//  TraxMobileTests
+//
+//  Created by  OPSITMAC  on 5/24/13.
+//  Copyright (c) 2013  OPSITMAC . All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TraxMobileTests : SenTestCase
+
+@end
---
+++ TraxMobileTests/TraxMobileTests.m
@@ -0,0 +1,32 @@
+//
+//  TraxMobileTests.m
+//  TraxMobileTests
+//
+//  Created by  OPSITMAC  on 5/24/13.
+//  Copyright (c) 2013  OPSITMAC . All rights reserved.
+//
+
+#import "TraxMobileTests.h"
+
+@implementation TraxMobileTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TraxMobileTests");
+}
+
+@end
